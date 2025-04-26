8e210246a9c2f66bc6f5f2dd98324bd1f27295e3
Jace Kim <jacekim.theal@gmail.com>
2025-04-26 18:36:31 +0900

    실습1. FastAPI Start

--- code/prac01/main_01.py
+++ code/prac01/main_01.py
@@ -1,17 +1,12 @@
 '''
 # 실습1. FastAPI Start 
-step01. 
 1. Import FastAPI and Union from typing
 2. Create an instance of FastAPI
 3. Define a root endpoint that returns a greeting message
 4. Define an endpoint to read an item by its ID and an optional query parameter
-
-step02. controller 추가
-
 '''
 from fastapi import FastAPI
 from typing import Union
-from controller import items
 
 app = FastAPI()
 
